c7214412e6b6727914ed483b3b43e65fb56e5622
Nickalus12 <nickalusbrewer@gmail.com>
2026-07-25 01:12:20 -0500

    Rebrand editor as Scratch Together

--- scratch-gui/webpack.config.js
+++ scratch-gui/webpack.config.js
@@ -186,7 +186,7 @@ module.exports = [
                 chunks: ['editor'],
                 template: 'src/playground/index.ejs',
                 filename: 'editor.html',
-                title: `${APP_NAME} - Run Scratch projects faster`,
+                title: `${APP_NAME} - code with your family`,
                 isEditor: true,
                 ...htmlWebpackPluginCommon
             }),
@@ -194,14 +194,14 @@ module.exports = [
                 chunks: ['player'],
                 template: 'src/playground/index.ejs',
                 filename: 'index.html',
-                title: `${APP_NAME} - Run Scratch projects faster`,
+                title: `${APP_NAME} - code with your family`,
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['fullscreen'],
                 template: 'src/playground/index.ejs',
                 filename: 'fullscreen.html',
-                title: `${APP_NAME} - Run Scratch projects faster`,
+                title: `${APP_NAME} - code with your family`,
                 ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
